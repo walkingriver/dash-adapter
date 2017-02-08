69fd88c860e30172ca10abd8cd4b2d12f17c352e
benjcallaghan <benjcallaghan@users.noreply.github.com>
2017-02-08 01:13:24 -0700

    Refactoring for code reuse (#4)
    * Extracted the conversions in validateAddress
    Created a new utilities module for all future conversions.
    
    * Replaced routing paths with config objects.
    This will allow the future use of routing params (e.g. get/:id)
    
    * Extracted the conversion functions for addAddress
    
    * Extracted conversion methods from provisionAddress
    
    * Extracted get method that includes logging
    
    * Extracted post method that includes logging
    
    * Simplified authCheck route

---
+++ converters.js
@@ -0,0 +1,127 @@
+var parser = require('xml2json');
+var _ = require('lodash');
+
+var validateAddress = {};
+validateAddress.createXmlString = function(obj) {
+    var address = {
+        validateLocation: {
+            location: {
+                address1: { $t: obj.addressLine1 },
+                address2: { $t: obj.addressLine2 },
+                community: { $t: obj.community },
+                state: { $t: obj.state },
+                postalcode: { $t: obj.postalCode },
+                type: { $t: 'ADDRESS' }
+            }
+        }
+    };
+    var xml = parser.toXml(address);
+    return xml;
+}
+validateAddress.createJsObject = function(xml) {
+    var json = parser.toJson(xml, { object: true });
+    var location = json['ns2:validateLocationResponse'].Location; 
+
+    var address = {
+      addressId: location.locationid['xsi:nil'] ? null : location.locationid,
+      addressLine1: location.address1,
+      addressLine2: _.isEmpty(location.address2) ? '' : location.address2,
+      houseNumber: location.legacydata.housenumber,
+      prefixDirectional: location.legacydata.predirectional,
+      streetName: location.legacydata.streetname,
+      //postDirectional: Unknown. Not in the Bandwidth response
+      //streetSuffix: Unknown. Not in the Bandwidth response
+      community: location.community,
+      state: location.state,
+      //unitType: Unknown. Not in the Bandwidth response
+      //unitTypeValue: Unknown. Not in the Bandwidth response
+      longitude: location.longitude,
+      latitude: location.latitude,
+      postalCode: location.postalcode,
+      zipPlusFour: location.plusfour,
+      //description: Unknown. Description found in response: "Location is geocoded"
+      addressStatus: location.status.code,
+      //createdOn: Unknown. activatedtime/updatetime found in response
+      //modifiedOn: Unknown. activatedtime/updatetime found in response
+    };
+
+    return address;
+}
+
+var addAddress = {};
+addAddress.createXmlString = function(obj) {
+    var address = {
+        addLocation: {
+            uri: {
+                callername: { $t: obj.endpoint.callerName },
+                uri: { $t: obj.endpoint.did },
+            },
+            location: {
+                address1: { $t: obj.addressLine1 },
+                address2: { $t: obj.addressLine2 },
+                callername: { $t: obj.endpoint.callerName },
+                community: { $t: obj.community },
+                postalcode: { $t: obj.postalCode },
+                state: { $t: obj.state },
+                type: { $t: 'ADDRESS' },
+            }
+        }
+    };
+    var xml = parser.toXml(address);
+    return xml;
+};
+addAddress.createJsObject = function(xml, did) {
+    var json = parser.toJson(xml, { object: true });
+    var location = json['ns2:addLocationResponse'].Location; 
+
+    var address = {
+      addressId: location.locationid, 
+      addressLine1: location.address1,
+      addressLine2: location.address2['xsi:nil'] ? null : location.address2,
+      houseNumber: location.legacydata.housenumber,
+      prefixDirectional: location.legacydata.predirectional,
+      streetName: location.legacydata.streetname,
+      //postDirectional: '', Unknown. Not in the Bandwidth response
+      //streetSuffix: '', Unknown. Not in the Bandwidth response
+      community: location.community,
+      state: location.state,
+      //unitType: '', Unknown. Not in the Bandwidth response
+      //unitTypeValue: '', Unknown. Not in the Bandwidth response
+      longitude: location.longitude,
+      latitude: location.latitude,
+      postalCode: location.postalcode,
+      zipPlusFour: location.plusfour,
+      //description: '', Unknown. Description found in response: "Location is geocoded"
+      addressStatus: location.status.code,
+      //createdOn: '', Unknown. activatedtime/updatetime found in response
+      //modifiedOn: '', Unknown. activatedtime/updatetime found in response
+      endpoint: {
+        did: did, // Not found in the Bandwidth response, but was part of the request
+        callerName: location.callername
+      }
+    };
+
+    return address;
+};
+
+var provisionAddress = {};
+provisionAddress.createXmlString = function(obj) {
+    var locationId = {
+        provisionLocation: {
+            locationid: { $t: obj } // Function takes a single int as a request
+        }
+    };
+    var xml = parser.toXml(locationId);
+    return xml;
+};
+provisionAddress.createJsObject = function(xml) {
+    var json = parser.toJson(xml, { object: true });
+    var status = json['ns2:provisionLocationResponse'].LocationStatus;
+    return status.code; // Function returns only a single string;
+};
+
+module.exports = {
+    validateAddress,
+    addAddress,
+    provisionAddress
+};
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var config = require('./config/config');
 var rp = require('request-promise');
 var restify = require('restify');
 var parser = require('xml2json');
-var _ = require('lodash');
+var converters = require('./converters');
+var bandwidth = require('./bandwidth');
 var options = {
   'auth': {
     'sendImmediately': true
@@ -29,199 +30,71 @@ server.use(function (req, res, next) {
   }
 });
 
-server.post(/ValidateAddress/i, validateAddress);
-server.post(/AddAddress/i, addAddress);
-server.post(/ProvisionAddress/i, provisionAddress);
+server.post({ path: '/ValidateAddress', flags: 'i' }, validateAddress);
+server.post({ path: '/AddAddress', flags: 'i' }, addAddress);
+server.post({ path: '/ProvisionAddress', flags: 'i' }, provisionAddress);
 
-
-server.get('/', function (req, res, next) {
-  // console.log(req);
-  authCheck(req, res, next);
-});
-
+server.get('/', authCheck);
 
 server.listen(config.port, function () {
   console.log('Listening on ', config.port);
 });
 
 function validateAddress(req, res, next) {
-  //res.send('Not Yet Implemented.');
-  //res.send(req.body);
-  var address = {
-    validateLocation: {
-      location: {
-        address1: { $t: req.body.addressLine1 },
-        address2: { $t: req.body.addressLine2 },
-        community: { $t: req.body.community },
-        state: { $t: req.body.state },
-        postalcode: { $t: req.body.postalCode },
-        type: { $t: 'ADDRESS' }
-      }
-    }
-  };
-  var xml = parser.toXml(address);
-  rp.post(config.dash.url + 'validatelocation', {
-    auth: options.auth,
-    body: xml,
-    headers: [
-        {
-          name: 'content-type',
-          value: 'application/xml'
-        }
-      ]
-  })
+  var converter = converters.validateAddress;
+
+  var xml = converter.createXmlString(req.body);
+  bandwidth.post(config.dash.url + 'validatelocation', options, xml)
   .then(function (response) {
-    var json = parser.toJson(response, { object: true });
-    var location = json['ns2:validateLocationResponse'].Location; 
-    console.log(location);
-
-    var validatedAddress = {
-      //addressId: Unknown. Not in the Bandwidth response
-      addressLine1: location.address1,
-      addressLine2: _.isEmpty(location.address2) ? '' : location.address2,
-      houseNumber: location.legacydata.housenumber,
-      prefixDirectional: location.legacydata.predirectional,
-      streetName: location.legacydata.streetname,
-      //postDirectional: Unknown. Not in the Bandwidth response
-      //streetSuffix: Unknown. Not in the Bandwidth response
-      community: location.community,
-      state: location.state,
-      //unitType: Unknown. Not in the Bandwidth response
-      //unitTypeValue: Unknown. Not in the Bandwidth response
-      longitude: location.longitude,
-      latitude: location.latitude,
-      postalCode: location.postalcode,
-      zipPlusFour: location.plusfour,
-      //description: Unknown. Description found in response: "Location is geocoded"
-      addressStatus: location.status.code,
-      //createdOn: Unknown. activatedtime/updatetime found in response
-      //modifiedOn: Unknown. activatedtime/updatetime found in response
-    };
-
-    res.json(validatedAddress);
+    var obj = converter.createJsObject(response);
+    res.json(obj);
     next();
   })
   .catch(function (err) {
-    console.log('Error: ', err);
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    res.end(err);
+    next(err);
   });
 }
 
 function addAddress(req, res, next) {
-  var address = {
-    addLocation: {
-      uri: {
-        callername: { $t: req.body.endpoint.callerName },
-        uri: { $t: req.body.endpoint.did },
-      },
-      location: {
-        address1: { $t: req.body.addressLine1 },
-        address2: { $t: req.body.addressLine2 },
-        callername: { $t: req.body.endpoint.callerName },
-        community: { $t: req.body.community },
-        postalcode: { $t: req.body.postalCode },
-        state: { $t: req.body.state },
-        type: { $t: 'ADDRESS' },
-      }
-    }
-  };
+  var converter = converters.addAddress;
+  
+  var xml = converter.createXmlString(req.body);
 
-  rp.post(config.dash.url + 'addlocation', {
-    auth: options.auth,
-    body: parser.toXml(address),
-    headers: [
-        {
-          name: 'content-type',
-          value: 'application/xml'
-        }
-      ]
-  })
+  bandwidth.post(config.dash.url + 'addlocation', options, xml)
   .then(function (response) {
-    var json = parser.toJson(response, { object: true });
-    var responseLocation = json['ns2:addLocationResponse'].Location; 
-    console.log(responseLocation);
-
-    var location = {
-      addressId: responseLocation.locationid, 
-      addressLine1: responseLocation.address1,
-      addressLine2: responseLocation.address2['xsi:nil'] ? null : responseLocation.address2,
-      houseNumber: responseLocation.legacydata.housenumber,
-      prefixDirectional: responseLocation.legacydata.predirectional,
-      streetName: responseLocation.legacydata.streetname,
-      //postDirectional: '', Unknown. Not in the Bandwidth response
-      //streetSuffix: '', Unknown. Not in the Bandwidth response
-      community: responseLocation.community,
-      state: responseLocation.state,
-      //unitType: '', Unknown. Not in the Bandwidth response
-      //unitTypeValue: '', Unknown. Not in the Bandwidth response
-      longitude: responseLocation.longitude,
-      latitude: responseLocation.latitude,
-      postalCode: responseLocation.postalcode,
-      zipPlusFour: responseLocation.plusfour,
-      //description: '', Unknown. Description found in response: "Location is geocoded"
-      addressStatus: responseLocation.status.code,
-      //createdOn: '', Unknown. activatedtime/updatetime found in response
-      //modifiedOn: '', Unknown. activatedtime/updatetime found in response
-      endpoint: {
-        did: req.body.endpoint.did, // Not found in the Bandwidth response, but was part of the request
-        callerName: responseLocation.callername
-      }
-    };
-
+    // One property is missing from the XML response, but is contained in the original request.
+    var location = converter.createJsObject(response, req.body.endpoint.did);
     res.json(location);
     next();
   })
   .catch(function (err) {
-    console.log('Error: ', err);
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    res.end(err);
+    next(err);
   })
 }
 
 function provisionAddress(req, res, next) {
-  var locationId = {
-    provisionLocation: {
-      locationid: { $t: req.body } // Function takes a single int as a request
-    }
-  };
-  rp.post(config.dash.url + 'provisionlocation', {
-    auth: options.auth,
-    body: parser.toXml(locationId),
-    headers: [
-        {
-          name: 'content-type',
-          value: 'application/xml'
-        }
-      ]
-  })
+  var converter = converters.provisionAddress;
+  
+  var xml = converter.createXmlString(req.body);
+  bandwidth.post(config.dash.url + 'provisionlocation', options, xml)
   .then(function (response) {
-    var json = parser.toJson(response, { object: true });
-    var status = json['ns2:provisionLocationResponse'].LocationStatus; 
-    console.log(status);
-
-    res.send(status.code);
+    var status = converter.createJsObject(response);
+    res.send(status);
     next();
   })
   .catch(function (err) {
-    console.log('Error: ', err);
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    res.end(err);
+    next(err);
   })
 }
 
 function authCheck(req, res, next) {
-  rp.get(config.dash.url + 'authenticationcheck ', options)
+  bandwidth.get(config.dash.url + 'authenticationcheck ', options)
     .then(function (response) {
-      console.log('Response: ', response);
-      // res.writeHead(200, { 'Content-Type': 'text/xml' });
       res.send(response);
       next();
     })
     .catch(function (err) {
       // API call failed... 
-      console.log('Error: ', err);
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end(err);
+      next(err);
     });
 }
